b81c570478828fa1f38544ba71ebbe4ad7447f80
rainpraks <133800439+rainpraks@users.noreply.github.com>
2024-01-11 12:00:21 +0200

    kasutajad db-i korras

--- functions/register.go
+++ functions/register.go
@@ -12,7 +12,7 @@ var db *sql.DB
 
 func InitDb() {
 	var err error
-	db, err = sql.Open("sqlite3", "database.db")
+	db, err = sql.Open("sqlite3", "db/database.db")
 	if err != nil {
 		log.Fatal(err)
 	}
